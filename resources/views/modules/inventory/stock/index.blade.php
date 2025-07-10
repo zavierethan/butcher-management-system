487e01e9b830748d28cadb7afc4b650312057b1c
nooravipp-ux <nooravipp9@gmail.com>
2025-07-11 01:03:48 +0700

    dsd

--- resources/views/modules/inventory/stock/index.blade.php
+++ resources/views/modules/inventory/stock/index.blade.php
@@ -175,7 +175,6 @@
                     return `
                         <div class="text-center">
                             <a href="/stock-logs/${row.id}" class="btn btn-sm btn-light btn-active-light-primary">Log Transaksi</a>
-                            <a href="/stocks/opnames/${row.id}" class="btn btn-sm btn-light btn-active-light-primary">Stock Opname</a>
                         <div>
                     `;
                 }
